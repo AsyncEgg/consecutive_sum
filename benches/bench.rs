e7187c6ddccf8670c2c486d499ef9bda51c88e33
Egg55 <emilianomilanog@gmail.com>
2023-06-27 21:31:48 -0600

    bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -82,3 +82,30 @@ fn criterion_benchmark(c: &mut Criterion) {
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
+
+
+//consecutive iter        time:   [106.56 ns 106.67 ns 106.80 ns]
+//                        change: [+2.4875% +2.8795% +3.3182%] (p = 0.00 < 0.05)
+//                        Performance has regressed.
+//Found 2 outliers among 100 measurements (2.00%)
+//  1 (1.00%) high mild
+//  1 (1.00%) high severe
+//
+//consecutive for         time:   [32.611 ns 32.885 ns 33.150 ns]
+//                        change: [-5.5711% -4.6678% -3.7300%] (p = 0.00 < 0.05)
+//                        Performance has improved.
+//Found 3 outliers among 100 measurements (3.00%)
+//  1 (1.00%) low mild
+//  2 (2.00%) high mild
+//
+//consecutive iter better time:   [36.660 ns 36.761 ns 36.860 ns]
+//                        change: [-0.5196% +0.7816% +1.8608%] (p = 0.21 > 0.05)
+//                        No change in performance detected.
+//Found 5 outliers among 100 measurements (5.00%)
+//  3 (3.00%) low mild
+//  2 (2.00%) high mild
+//
+//consecutive weird       time:   [5.0314 ns 5.0360 ns 5.0417 ns]
+//Found 9 outliers among 100 measurements (9.00%)
+//  3 (3.00%) high mild
+//  6 (6.00%) high severe
